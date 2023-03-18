79f9e14667d1cbaea05fcc76d7186d861a17110d
wdd <296151852@qq.com>
2023-03-18 18:14:04 +0800

    1

--- node-express-serve/README.md
+++ node-express-serve/README.md
@@ -2,7 +2,4 @@
     npm i
     npm start
 
-# 热重启
-    "start": "nodemon ./bin/www"
-    
     http://localhost:3000/
--- node-express-serve/routes/index.js
+++ node-express-serve/routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  let data = {
+    code: 1,
+    message: "ok",
+    result: {
+      list: [{name:"张三",age:20}],
+      page: 1,
+      pageSize: 10,
+      total: 1
+    }
+  }
+  res.send(data)
 });
 
 module.exports = router;
